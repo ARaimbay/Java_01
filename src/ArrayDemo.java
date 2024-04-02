0d4739f9d3e8028b0cc8819b8bf90de77244e70f
Aigerim Raimbay <raimbay89@gmail.com>
2024-04-02 18:48:33 -0500

    array with integers

--- src/ArrayDemo.java
+++ src/ArrayDemo.java
@@ -1,6 +1,10 @@
 public class ArrayDemo {
     public static void main(String[] args) {
         System.out.println(args.length);
-        System.out.println(args[0]);
+//      System.out.println(args[1]);
+
+        int[] nums = {10, 20, 30, 40, 50};
+        System.out.println(nums.length);
+        System.out.println(nums[1]);
     }
 }
