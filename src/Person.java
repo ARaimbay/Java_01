7f4eb35593de060ba0116ac760226031539aad1e
Aigerim Raimbay <raimbay89@gmail.com>
2024-04-03 17:08:03 -0500

    person class modified

--- src/Person.java
+++ src/Person.java
@@ -1,11 +1,23 @@
 public class Person {
+    private String middleName = "Christopher";
 
     public void sayHello() {
         System.out.println("Hello");
     }
 
+    public void saySomething(String message){
+        System.out.println(message);
+    }
+
+    public char getMiddleInitial() {
+        return middleName.charAt(0);
+    }
+
     public static void main(String[] args) {
         Person p1 = new Person();
         p1.sayHello();
+        p1.saySomething("My groovy message");
+        p1.saySomething("Here's something else to say");
+        System.out.println(p1.getMiddleInitial());
     }
 }
